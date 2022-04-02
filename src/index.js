81031e7d306643e65d9eab56a4154f6f09394931
fedeghe <fedeghe@gmail.com>
2022-04-02 10:49:20 +0200

    wip

--- src/index.js
+++ src/index.js
@@ -3,9 +3,12 @@ function () {
     const BASE = 10;
 
     function Bignumx(n) {
-        this.num = "" + n;
-        this.sign = [1, -1][this.num.match(/^-/)];
-        this.arr = this.num.split('').reverse();
+        if (typeof n !== 'string') throw 'String expected'
+        
+        
+        this.sign = n.match(/^-/) ? -1 : 1;
+        this.num = this.sign === -1 ? n.substring(1) : n;
+        this.arr = this.num.split('').reverse().map(n => parseInt(n, BASE));
     }
 
     Bignumx.prototype.toString = function () {
@@ -15,20 +18,38 @@ function () {
         var carry = 0,
             i = 0,    
             res = [],
-            tmp;
-        while (typeof bn[i] !== 'undefined' || typeof this.arr[i] !== 'undefined') {
-            tmp = carry + bn.sign * (bn[i] || 0) + this.sign * (this.arr[i] || 0);
-            carry = parseInt(tmp / 10, 10);
-            res[i] = tmp % 10;
+            tmp,
+            sign = 1,
+            bnSmaller = null;
+        while (typeof bn.arr[i] !== 'undefined' || typeof this.arr[i] !== 'undefined') {
+            tmp = Math.abs(carry + bn.sign * (bn.arr[i] || 0) +  this.sign * (this.arr[i] || 0));
+            if (bnSmaller === null) {
+                if (bn.arr[i] > this.arr[i]) bnSmaller = false
+                if (bn.arr[i] < this.arr[i]) bnSmaller = true
+            }
+            carry = parseInt(tmp / BASE, BASE);
+            res[i] = tmp % BASE;
             i++;
         }
-        console.log(res)
-        return new BigNumx()
-        
+        if (bn.sign === -1 && this.sign === -1) sign = -1
+        // already default
+        // if (bn.sign === 1 && this.sign === 1) sign = 1 
+        if (sign === 1 && bnSmaller !== null) {
+            // true B < t => neg <=> -t+B
+            if (bnSmaller) {
+                if (this.sign === -1 && bn.sign === 1) sign = -1
+            // false B > t => neg <=> t-B
+            } else {
+                if (this.sign === 1 && bn.sign === -1) sign = -1
+            }
+        }
+
+        if (carry) res.push(carry)
+        return new Bignumx((sign === 1 ? '' : '-') + res.reverse().join(''))
     };
     Bignumx.prototype.toString = function () {
-        return this.n;
+        return (this.sign === -1 ? '-' : '' ) + this.num;
     };
 
-    return Bignumx;
+    return function (n) {return new Bignumx(n)};
 }
--- test/test1.js
+++ test/test1.js
@@ -1,9 +1,41 @@
-var b = require('../index').Bignumx,
+var b = require('../index.js'),
     assert = require('assert');
+console.log(b)
 
 describe('constructor', () => {
     it('should convert as expected', () => {
-        var big = b(10)
+        var big = b('10')
         assert.equal(typeof big, "object");
+    })
+    it('should convert as', () => {
+        var big = b('10')
+        assert.equal(big, "10");
+    })
+    it('should sum as expected positive', () => {
+        var big1 = b('10'),
+            big2 = b('222165151435165135135135'),
+            s = big1.sum(big2);
+        assert.equal(s, "222165151435165135135145");
+    })
+    it('should sum as expected negative', () => {
+        var big1 = b('23423423422'),
+            big2 = b('-222'),
+            s = big1.sum(big2);
+
+        assert.equal(s.toString(), "23423423200");
+    })
+    it('should sum as expected negative 2', () => {
+        var big1 = b('-23423423422'),
+            big2 = b('-222'),
+            s = big1.sum(big2);
+
+        assert.equal(s.toString(), "-23423423644");
+    })
+    it('should sum as expected negative 3', () => {
+        var big1 = b('-23423423422'),
+            big2 = b('222'),
+            s = big1.sum(big2);
+
+        assert.equal(s.toString(), "-23423423200");
     })
 })
